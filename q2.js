22f3124ccfa58966a4608970d84b8344209bd91f
space-boop31 <arpitasrivastava000@gmail.com>
2025-04-03 20:19:13 +0530

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,10 @@
+let x = 10;
+let y = 5;
+
+let addition = x + y;       // Addition
+let multiplication = x * y; // Multiplication
+let modulus = x % y;        // Modulus
+
+console.log("Addition:", addition);       
+console.log("Multiplication:", multiplication); 
+console.log("Modulus:", modulus);
