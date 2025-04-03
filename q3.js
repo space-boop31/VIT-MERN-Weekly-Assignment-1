96195d3ab7837a1582a225f5bf8a917fda099563
space-boop31 <arpitasrivastava000@gmail.com>
2025-04-03 20:29:36 +0530

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,5 @@
+let n = 7; // Change this number to test with different values
+
+let result = (n % 2 === 0) ? "Even" : "Odd";
+
+console.log("The number is:", result);
---
+++ q4.js
@@ -0,0 +1,7 @@
+let numbers = [];
+
+for (let i = 1; i <= 5; i++) {
+    numbers.push(i);
+}
+
+console.log(numbers); // Output: [1, 2, 3, 4, 5]
